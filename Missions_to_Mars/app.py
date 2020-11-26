58a3c7910a40ce99a27d8fdb52cf26bab37dfcf0
Supasyl <69235823+Supasyl@users.noreply.github.com>
2020-11-26 18:03:16 +0800

    Final submission

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -10,56 +10,24 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/Mars_db")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
+def index():
 
     # Find one record of data from the mongo database
-    collection = mongo.db.collection.find_one()
-    # Return template and data
-    return render_template("index.html", collection=collection)
+    articles_images = mongo.db.articles_images.find_one()
+    print(articles_images)
 
-    # Find the feature image from the mongo database
-    images = mongo.db.images.find()
     # Return template and data
-    return render_template("index.html", images=images)
+    return render_template("index.html", articles_images=articles_images)
 
 # Route that will trigger the scrape function
-@app.route("/scrape")
+@app.route('/scrape')
 def scrape():
-
-    # Run the scrape function
-    collection = mongo.db.collection
-    article = scrape_mars.scrape_info()
-    # Update the Mongo database using update and upsert=True
-    collection.update({}, article, upsert=True)
-
-    # Run the scrape function
-    images = mongo.db.images
-    feature_image_url = scrape_mars.scrape_info2()
-    # Update the Mongo database using update and upsert=True
-    images.update({}, feature_image_url, upsert=True)
+    articles_images = mongo.db.articles_images
+    data = scrape_mars.scrape_all()
+    articles_images.update({}, data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
 
-
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-# # Define database and collection
-# db = client.Mars_db
-# collection = db.articles
-# images = db.images
-#     # Insert dictionary into MongoDB as a document
-#         collection.insert_one(post)
-# # Display items in MongoDB collection
-# list = db.articles.find()
-
-# for listing in list:
-#     print(listing)
-
-# images.insert_one(post)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -14,7 +14,17 @@ def init_browser():
     executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
     return Browser('chrome', **executable_path, headless=False)
 
-def scrape_info():
+def scrape_all():
+    articles_images ={}
+    articles_images['article'] = scrape_article()
+    articles_images['feature_image'] = scrape_feature_image()
+    articles_images['cerberus'] = scrape_cerberus()
+    articles_images['schiaparelli'] = scrape_schiaparelli()
+    articles_images['syrtis_major'] = scrape_syrtis_major()
+    articles_images['valles_marineris'] = scrape_valles_marineris()
+    return articles_images
+
+def scrape_article():
     browser = init_browser()
     # define website and open in browser window
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
@@ -27,25 +37,25 @@ def scrape_info():
     soup = bs(html, 'html.parser')
 
     # Retrieve the parent divs for first page
-    articles = soup.find_all(class_='list_text')
+    articles1 = soup.find_all(class_='list_text')
 
     # loop over results to get article data
-    for article in articles:
+    for article in articles1:
         # scrape the article title
         news_title = article.find(class_='content_title').text
         # scrape the article paragraph
         news_p = article.find(class_='article_teaser_body').text
         # add articles into dictionary
-        collection = {
+        articles = {
             'Title': news_title,
             'Paragraph': news_p
         }
         
     # Close the browser after scraping
     browser.quit()
-    return collection
+    return articles
 
-def scrape_info2():
+def scrape_feature_image():
     browser = init_browser()
     # define website and open in browser window
     url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
@@ -76,3 +86,120 @@ def scrape_info2():
     } 
 
     return images
+
+def scrape_cerberus():
+    browser = init_browser()
+    # define website and open in browser window
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    # time to load the page
+    time.sleep(5)
+
+    # get to the page with the image url
+    browser.links.find_by_partial_text('Cerberus Hemisphere').first.click()
+
+    # scrape page into Soup
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    
+    # Identify and return the images
+    results = soup.select_one('img.wide-image').get('src')
+
+    # Close the browser after scraping
+    browser.quit()
+
+    # store the image url
+    images = {
+        'cerberus_image_url': f'https://astrogeology.usgs.gov{results}'
+    } 
+
+    return images
+
+def scrape_schiaparelli():
+    browser = init_browser()
+    # define website and open in browser window
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    # time to load the page
+    time.sleep(5)
+
+    # get to the page with the image url
+    browser.links.find_by_partial_text('Schiaparelli Hemisphere').first.click()
+
+    # scrape page into Soup
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    
+    # Identify and return the images
+    results = soup.select_one('img.wide-image').get('src')
+
+    # Close the browser after scraping
+    browser.quit()
+
+    # store the image url
+    images = {
+        'schiaparelli_image_url': f'https://astrogeology.usgs.gov{results}'
+    } 
+
+    return images
+
+def scrape_syrtis_major():
+    browser = init_browser()
+    # define website and open in browser window
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    # time to load the page
+    time.sleep(5)
+
+    # get to the page with the image url
+    browser.links.find_by_partial_text('Syrtis Major Hemisphere').first.click()
+
+    # scrape page into Soup
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    
+    # Identify and return the images
+    results = soup.select_one('img.wide-image').get('src')
+
+    # Close the browser after scraping
+    browser.quit()
+
+    # store the image url
+    images = {
+        'syrtis_major_image_url': f'https://astrogeology.usgs.gov{results}'
+    } 
+
+    return images
+
+def scrape_valles_marineris():
+    browser = init_browser()
+    # define website and open in browser window
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    # time to load the page
+    time.sleep(5)
+
+    # get to the page with the image url
+    browser.links.find_by_partial_text('Valles Marineris Hemisphere').first.click()
+
+    # scrape page into Soup
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    
+    # Identify and return the images
+    results = soup.select_one('img.wide-image').get('src')
+
+    # Close the browser after scraping
+    browser.quit()
+
+    # store the image url
+    images = {
+        'valles_marineris_image_url': f'https://astrogeology.usgs.gov{results}'
+    } 
+
+    return images
+
